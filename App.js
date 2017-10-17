024fbfdbb3bbb25ddd7e50761de98d11ed2718ab
Carlos Guillen <carlos@applab.do>
2017-10-17 12:40:25 -0400

    Added react-navigation, Created login form and signup form

--- App.js
+++ App.js
@@ -1,36 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import { Platform } from 'react-native';
-import { Container, Header, Content, Button, Form, Input } from 'native-base';
+import { Platform, StyleSheet } from 'react-native';
+import { Container, Content} from 'native-base';
+import Login from './views/Forms/Login';
+import SignUp from './views/Forms/SignUp';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import { StackNavigator } from 'react-navigation';
+
+
+const SimpleApp = StackNavigator({
+  Login: { screen: Login },
+  SignUp: { screen: SignUp }
 });
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <Container>
-        <Header />
-        <Content>
-        <Form>
-        <Item>
-          <Input placeholder="Username" />
-        </Item>
-        <Item last>
-          <Input placeholder="Password" />
-        </Item>
-      </Form>
-        </Content>
-      </Container>
+      <SimpleApp/>
     );
   }
 }
