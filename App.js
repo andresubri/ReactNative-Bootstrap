9966ff2fce8d2fbc56ae19b661048d6b57cf0a17
Carlos Andres <andriusic@gmail.com>
2017-10-17 19:12:26 -0400

    Changed restore password form from modal to view

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import { Platform, StyleSheet } from 'react-native';
 import { Container, Content} from 'native-base';
 import Login from './views/Forms/Login';
 import SignUp from './views/Forms/SignUp';
+import RestorePassword from './views/Forms/RestorePassword';
 
 import { StackNavigator } from 'react-navigation';
 
 
 const SimpleApp = StackNavigator({
   Login: { screen: Login },
-  SignUp: { screen: SignUp }
+  SignUp: { screen: SignUp },
+  RestorePassword: { screen: RestorePassword },
+  
 });
 
 export default class App extends Component<{}> {
--- views/Forms/Login.js
+++ views/Forms/Login.js
@@ -2,48 +2,41 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet } from 'react-native';
 import { Form, Item, Label, Input, Button, Icon, Title, Text, Content, Container } from 'native-base';
 import * as strings from '../../Data/Strings';
-import RestorePassword from '../CardDialog';
+import RestorePassword from './RestorePassword';
 
 export default class Login extends Component {
   static navigationOptions = {
     header: null
   }
 
-  showRestorePassword = function () {
-    
-  }
-
   render() {
-    const { navigate } = this.props.navigation;    
+    const { navigate } = this.props.navigation;
     return (
-      <RestorePassword/>
-      
-      /*       <Container style={styles.content}>
-      <Content >
-        <Form>
-          <Item floatingLabel>
-            <Label>{strings.Email}</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel last>
-            <Label>{strings.Password}</Label>
-            <Input />
-          </Item>
-          <Button full danger>
-            <Text>{strings.Login}</Text>
+      <Container style={styles.content}>
+        <Content >
+          <Form>
+            <Item floatingLabel>
+              <Label>{strings.Email}</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>{strings.Password}</Label>
+              <Input />
+            </Item>
+            <Button full danger>
+              <Text>{strings.Login}</Text>
+            </Button>
+          </Form>
+          <Button transparent full
+            onPress={() => navigate('SignUp')}>
+            <Text>{strings.SignUp}</Text>
           </Button>
-        </Form>
-        <Button transparent full
-        onPress={() => navigate('SignUp')}>
-        <Text>{strings.SignUp}</Text>
-        </Button>
-        <Button transparent full
-        onPress={() => showRestorePassword() }>
-
-          <Text>{strings.RestorePassword}</Text>
-        </Button>
-      </Content>      
-    </Container> */
+          <Button transparent full
+          onPress={() => navigate('RestorePassword')}>
+            <Text>{strings.RestorePassword}</Text>
+          </Button>
+        </Content>
+      </Container>
     );
   }
 }
@@ -51,5 +44,5 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
   content: {
     padding: 10
-    }
+  }
 });
